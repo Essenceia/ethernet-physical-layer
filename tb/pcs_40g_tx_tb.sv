72aebabc7bbdc1913a81aed1d33731fb4f2797cc
Julia Desmazes <julia.desmazes@gmail.com>
2023-07-30 22:34:31 -0700

    RTL,TB: Passed tb to drive multiples lanes

--- tb/pcs_40g_tx_tb.sv
+++ tb/pcs_40g_tx_tb.sv
@@ -3,7 +3,7 @@
 `endif
 module pcs_40g_tx_tb;
 
-localparam LANE_N = 1;
+localparam LANE_N = 4;
 localparam DATA_W = 64;
 localparam KEEP_W = $clog2(DATA_W);
 	
@@ -26,21 +26,27 @@ logic [PMA_CNT_N*PMA_DATA_W-1:0] pma_o;
 logic [PMA_CNT_N*PMA_DATA_W-1:0] tb_pma;
 
 // debug id
-logic [DEBUG_ID_W-1:0] tb_debug_id;
+logic [LANE_N*DEBUG_ID_W-1:0] tb_debug_id;
 
 reg   clk = 1'b0;
 logic nreset;
 
 always clk = #5 ~clk;
 
-task new_block();
-	// next block driver
-	$tb(ctrl_v_i, idle_v_i, start_v_i,term_v_i, keep_i, 
-		err_v_i , data_i);	
-	// expected result
-	$tb_exp(tb_pma, tb_debug_id);
-endtask
-
+genvar i;
+generate
+for( i=0; i < LANE_N; i++) begin
+always @(posedge clk) begin
+	if( nreset ) begin
+		// next block driver
+		$tb(ctrl_v_i[i], idle_v_i[i], start_v_i[i],term_v_i[i], keep_i[i*KEEP_W+KEEP_W-1:i*KEEP_W], 
+		err_v_i[i] , data_i[i*DATA_W+DATA_W-1:i*DATA_W]);	
+			// expected result
+		$tb_exp(tb_pma[i*DATA_W+DATA_W-1:i*DATA_W], tb_debug_id[i*DEBUG_ID_W+DEBUG_ID_W-1:i*DEBUG_ID_W]);
+	end
+end
+end
+endgenerate
 initial begin
 	$dumpfile("build/wave.vcd");
 	$dumpvars(0, pcs_40g_tx_tb);
@@ -49,7 +55,7 @@ initial begin
 	nreset = 1'b1;
 	
 	for ( int i= 0; i < `TB_LOOP_CNT_N; i ++ ) begin
-		new_block();		
+		//new_block();		
 		#10
 		`ifdef DEBUG
 		$display("Seq cnt %d, align marker gap %d", m_pcs_40g_tx.seq_q, m_pcs_40g_tx.m_align_market.gap_q );
