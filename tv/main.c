53838d3004909f9cb1f7a735ec05d89144803b4b
Julia Desmazes <julia.desmazes@gmail.com>
2023-08-04 16:32:57 -0700

    TV: Fix 64b66b code for clarity, producing test vectors

--- tv/main.c
+++ tv/main.c
@@ -2,21 +2,23 @@
 #include <stdio.h>
 #include <byteswap.h>
 
+#define I1 57
+#define I0 38
+
 int main(){
 	uint64_t reg  = UINT64_MAX;
 	uint64_t test = 0x1e;
 	uint64_t res  = 0;
+	uint64_t t  = 0;
 	uint64_t i57, i38, ti0;
-	printf("64b66b scramble\ninitial values:\n"
-	"reg %+"PRIx64"\ninput %+"PRIx64"\n", reg, test);	
+	printf("in: %016lx\nstate: %016lx\n", test, reg);	
 	for( int i=0; i<64; i++){
-		i57 = ( reg >> 57 ) & 0x01;
-		i38 = ( reg >> 38 ) & 0x01;
-		ti0 = test & 0x01;
-		test >>= 1;
-		res <<= 1;
-		res |= ( 0x01 & ( i57 ^ i38 ^ ti0 ));
-		reg = (reg << 1)| ( res & 0x01 );
+		i57 = ( reg >> I1 ) & 0x01;
+		i38 = ( reg >> I0 ) & 0x01;
+		ti0 = ( test >> i)  & 0x01;
+		t = ( i57 ^ i38 ) ^ ti0 ;
+		res |= t << i;
+		reg = (reg << 1)| t;
 	}
-	printf("output:\nres %+"PRIx64"\nres(le) %+"PRIx64"\nreg %+"PRIx64"\n", res, bswap_64(res), reg);	
+	printf("output:%016lx\n",  res);
 }
