cb3c8d94e247e8c3beb9522cccce9a3cddaed2dd
Julia Desmazes - ws <julia.desmazes@gmail.com>
2023-09-26 11:36:21 -0700

    TCL,MAKE: Added clean command for quartus project files, updated tcl script

--- tcl/quartus_setup.tcl
+++ tcl/quartus_setup.tcl
@@ -4,16 +4,21 @@ load_package ::quartus::project
 
 set project_name "PCS"
 
-set fpga_family "Cyclone 10 GX"
+set fpga_family "Cyclone 10"
 
-set fpga_part "10CX150YF780"
+set fpga_device 10CX150YF780
 
 set project_dir "quartus_test"
 
-set_global_assignment -name PROJECT_OUTPUT_DIRECTORY $project_dir
+
+# create new quartus project
+project_new $project_name -overwrite
+
+# set decide
+set_global_assignment -name DEVICE $fpga_device
 
 # project configuration
 set_global_assignment -name OPTIMIZATION_MODE "AGGRESSIVE PERFORMANCE"
+set_global_assignment -name PROJECT_OUTPUT_DIRECTORY $project_dir
 
-# create new quartus project
-project_new -family $fpga_family -overwrite -part $fpga_part $project_name
+
