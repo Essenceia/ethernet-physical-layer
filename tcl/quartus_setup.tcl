56d361e06288ac97b7b11a83c4d1fa67b0e82462
Julia Desmazes <julia.desmazes@gmail.com>
2023-09-26 11:00:38 -0700

    TCL: Update tcl script, including quartus project package

--- tcl/quartus_setup.tcl
+++ tcl/quartus_setup.tcl
@@ -1,3 +1,5 @@
+load_package ::quartus::project
+
 # Setup PCS project for quartus
 
 set project_name "PCS"
@@ -5,3 +7,13 @@ set project_name "PCS"
 set fpga_family "Cyclone 10 GX"
 
 set fpga_part "10CX150YF780"
+
+set project_dir "quartus_test"
+
+set_global_assignment -name PROJECT_OUTPUT_DIRECTORY $project_dir
+
+# project configuration
+set_global_assignment -name OPTIMIZATION_MODE "AGGRESSIVE PERFORMANCE"
+
+# create new quartus project
+project_new -family $fpga_family -overwrite -part $fpga_part $project_name
