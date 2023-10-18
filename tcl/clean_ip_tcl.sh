ec13a8f694007edd0426e7b141ebe35f4ef16825
Julia Desmazes - ws <julia.desmazes@gmail.com>
2023-10-18 13:16:09 -0700

    SH: Call clean IP TCL files when re-generating new ip when TCL file have been changed, automatizing process

--- tcl/clean_ip_tcl.sh
+++ tcl/clean_ip_tcl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # relax qsys requirement
-sed -i 's/package require -exact qsys.*/package require qsys/' *.tcl
+sed -i 's/package require -exact qsys.*/package require qsys/' $1
 
 # remove `set_project_property: BOARD`
-sed -i 's/set_project_property BOARD .*//' *.tcl
+sed -i 's/set_project_property BOARD .*//' $1
 
