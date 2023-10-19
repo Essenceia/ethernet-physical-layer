48e994c5ccafebf19c35cae62b25e1dd51509735
Julia Desmazes <julia.desmazes@gmail.com>
2023-10-19 16:05:21 -0700

    TB: Fix lint loopback tb

--- tb/pcs_10g_loopback_tb.sv
+++ tb/pcs_10g_loopback_tb.sv
@@ -6,17 +6,21 @@
  * This code is provided "as is" without any express or implied warranties. */ 
 
 `ifndef TB_LOOP_N
-`define TB_LOOP_N 5
+`define TB_LOOP_N 2
 `endif
 
 `define SYNC_CTRL 2'b10
 `define SYNC_DATA 2'b01	
 
+`define SET_RANDOM_HEAD \
+	/*verilator lint_off WIDTHTRUNC */ \
+	head = $random % 2 ? `SYNC_DATA : `SYNC_CTRL; \
+	/*verilator lint_on WIDTHTRUNC */ 
+
 /* pcs 10g loopback tb
  * Small test bench to check module behavior
  */
 module pcs_10g_loopback_tb;
-parameter IS_10G = 1;
 parameter DATA_W = 64;
 parameter HEAD_W = 2;
 parameter BLOCK_W = HEAD_W + DATA_W;
@@ -26,7 +30,10 @@ reg nreset;
 
 logic rx_locked_i;
 logic [DATA_W-1:0] rx_par_data_i;
+
+/* verilator lint_off UNUSEDSIGNAL */
 logic [DATA_W-1:0] tx_par_data_o;
+/* verilator lint_on UNUSEDSIGNAL */
 
 /*verilator lint_off BLKSEQ*/
 always #5 clk = ~clk;
@@ -36,21 +43,23 @@ always #5 clk = ~clk;
 task send_valid_block();
 	logic [BLOCK_W-1:0] buff;
 	logic [HEAD_W-1:0] head;
-	head = $random % 2 ? `SYNC_DATA : `SYNC_CTRL;
+	`SET_RANDOM_HEAD
 	buff = { $random, $random, head };	
 	
 	rx_par_data_i = buff[DATA_W-1:0];
 	buff = buff >> DATA_W;
 	for(int i=2; i < 64; i=i+2) begin
 		#10
-	 	rx_par_data_i = buff;	
-		head = $random % 2 ? `SYNC_DATA : `SYNC_CTRL;
+	 	rx_par_data_i = buff[DATA_W-1:0];	
+		`SET_RANDOM_HEAD
 		buff = { $random, $random, head };	
+		/* verilator lint_off WIDTHTRUNC */
 		rx_par_data_i = buff << i;
+		/* verilator lint_on WIDTHTRUNC */
 		buff = buff >> i; 
 	end
 	#10
-	rx_par_data_i = buff;
+	rx_par_data_i = buff[DATA_W-1:0];
 	#10
 	rx_par_data_i = {DATA_W{1'bx}};	
 endtask
@@ -73,10 +82,7 @@ initial begin
 end
 
 
-pcs_10g_loopback #(
-	IS_10G (IS_10G),
-	DATA_W(DATA_W)
-)m_pcs_10g_loopback(
+pcs_10g_loopback m_pcs_10g_loopback(
 	.rx_par_clk    (clk),
 	.tx_par_clk    (clk),
 	.nreset        (nreset),
