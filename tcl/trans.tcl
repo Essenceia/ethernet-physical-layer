21f8f91e5e993bd2a69715e99478acdbcdf1c7e8
Julia Desmazes - ws <julia.desmazes@gmail.com>
2023-10-30 11:35:11 -0700

    TCL: Changed tcl file for transiver IP generation for quartus 23.3, previous tcl file was not compatible

--- tcl/trans.tcl
+++ tcl/trans.tcl
@@ -1,10 +1,10 @@
-package require qsys
+package require -exact qsys 23.3
 
 # create the system "trans"
 proc do_create_trans {} {
 	# create the system
 	create_system trans
-	
+	set_project_property BOARD {}
 	set_project_property DEVICE {10CX150YF780E5G}
 	set_project_property DEVICE_FAMILY {Cyclone 10 GX}
 	set_project_property HIDE_FROM_IP_CATALOG {true}
@@ -13,7 +13,7 @@ proc do_create_trans {} {
 	# add HDL parameters
 
 	# add the components
-	add_instance xcvr_native_a10_0 altera_xcvr_native_a10 19.1
+	add_instance xcvr_native_a10_0 altera_xcvr_native_a10 19.1.1
 	set_instance_parameter_value xcvr_native_a10_0 {anlg_enable_rx_default_ovr} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {anlg_enable_tx_default_ovr} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {anlg_link} {sr}
@@ -67,25 +67,25 @@ proc do_create_trans {} {
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_pipe_rx_polarity} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_analog_reset_ack} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_bitslip} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_blk_lock} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_blk_lock} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_clr_errblk_count} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_clr_errblk_count_c10} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_crc32_err} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_data_valid} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_data_valid} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_align_clr} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_align_val} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_cnt} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_del} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_empty} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_full} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_insert} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_del} {1}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_empty} {1}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_full} {1}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_insert} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_pempty} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_pfull} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_fifo_rd_en} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_frame} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_frame_diag_status} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_frame_lock} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_highber} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_highber} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_enh_highber_clr_cnt} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_is_lockedtodata} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_rx_is_lockedtoref} {1}
@@ -112,15 +112,15 @@ proc do_create_trans {} {
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_analog_reset_ack} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_bitslip} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_fifo_cnt} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_fifo_empty} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_fifo_full} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_fifo_pempty} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_fifo_pfull} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_fifo_empty} {1}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_fifo_full} {1}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_fifo_pempty} {1}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_fifo_pfull} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_frame} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_frame_burst_en} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_enh_frame_diag_status} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_pma_clkout} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_pma_div_clkout} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_pma_div_clkout} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_pma_elecidle} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_pma_iqtxrx_clkout} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_port_tx_pma_qpipulldn} {0}
@@ -136,7 +136,7 @@ proc do_create_trans {} {
 	set_instance_parameter_value xcvr_native_a10_0 {enable_ports_pipe_hclk} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_ports_pipe_rx_elecidle} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_ports_pipe_sw} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enable_ports_rx_manual_cdr_mode} {1}
+	set_instance_parameter_value xcvr_native_a10_0 {enable_ports_rx_manual_cdr_mode} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_ports_rx_manual_ppm} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_ports_rx_prbs} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_simple_interface} {1}
@@ -144,14 +144,14 @@ proc do_create_trans {} {
 	set_instance_parameter_value xcvr_native_a10_0 {enable_split_interface} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_transparent_pcs} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enable_upi_pipeline_options} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_low_latency_enable} {1}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_pcs_pma_width} {40}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_pld_pcs_width} {40}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_64b66b_enable} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_low_latency_enable} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_pcs_pma_width} {32}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_pld_pcs_width} {66}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_64b66b_enable} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_bitslip_enable} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_blksync_enable} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_blksync_enable} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_crcchk_enable} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_descram_enable} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_descram_enable} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_dispchk_enable} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_frmsync_enable} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_frmsync_mfrm_length} {2048}
@@ -160,11 +160,11 @@ proc do_create_trans {} {
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rx_polinv_enable} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rxfifo_align_del} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rxfifo_control_del} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_rxfifo_mode} {Phase compensation}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_rxfifo_mode} {10GBase-R}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rxfifo_pempty} {2}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rxfifo_pfull} {23}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_rxtxfifo_double_width} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_64b66b_enable} {0}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_64b66b_enable} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_bitslip_enable} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_crcerr_enable} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_crcgen_enable} {0}
@@ -176,8 +176,8 @@ proc do_create_trans {} {
 	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_krfec_burst_err_len} {1}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_polinv_enable} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_randomdispbit_enable} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_scram_enable} {0}
-	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_scram_seed} {2.0}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_scram_enable} {1}
+	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_scram_seed} {2.88230376152e+17}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_tx_sh_err} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_txfifo_mode} {Phase compensation}
 	set_instance_parameter_value xcvr_native_a10_0 {enh_txfifo_pempty} {2}
@@ -266,7 +266,7 @@ proc do_create_trans {} {
 	set_instance_parameter_value xcvr_native_a10_0 {std_tx_polinv_enable} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {support_mode} {user_mode}
 	set_instance_parameter_value xcvr_native_a10_0 {tx_pma_clk_div} {1}
-	set_instance_parameter_value xcvr_native_a10_0 {tx_pma_div_clkout_divider} {33}
+	set_instance_parameter_value xcvr_native_a10_0 {tx_pma_div_clkout_divider} {0}
 	set_instance_parameter_value xcvr_native_a10_0 {validation_rule_select} {}
 	set_instance_property xcvr_native_a10_0 AUTO_EXPORT true
 
@@ -285,14 +285,13 @@ proc do_create_trans {} {
 	set_interface_property rx_cdr_refclk0 EXPORT_OF xcvr_native_a10_0.rx_cdr_refclk0
 	set_interface_property tx_serial_data EXPORT_OF xcvr_native_a10_0.tx_serial_data
 	set_interface_property rx_serial_data EXPORT_OF xcvr_native_a10_0.rx_serial_data
-	set_interface_property rx_set_locktodata EXPORT_OF xcvr_native_a10_0.rx_set_locktodata
-	set_interface_property rx_set_locktoref EXPORT_OF xcvr_native_a10_0.rx_set_locktoref
 	set_interface_property rx_is_lockedtoref EXPORT_OF xcvr_native_a10_0.rx_is_lockedtoref
 	set_interface_property rx_is_lockedtodata EXPORT_OF xcvr_native_a10_0.rx_is_lockedtodata
 	set_interface_property tx_coreclkin EXPORT_OF xcvr_native_a10_0.tx_coreclkin
 	set_interface_property rx_coreclkin EXPORT_OF xcvr_native_a10_0.rx_coreclkin
 	set_interface_property tx_clkout EXPORT_OF xcvr_native_a10_0.tx_clkout
 	set_interface_property rx_clkout EXPORT_OF xcvr_native_a10_0.rx_clkout
+	set_interface_property tx_pma_div_clkout EXPORT_OF xcvr_native_a10_0.tx_pma_div_clkout
 	set_interface_property tx_parallel_data EXPORT_OF xcvr_native_a10_0.tx_parallel_data
 	set_interface_property unused_tx_parallel_data EXPORT_OF xcvr_native_a10_0.unused_tx_parallel_data
 	set_interface_property rx_parallel_data EXPORT_OF xcvr_native_a10_0.rx_parallel_data
